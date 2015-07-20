3d614dfe29e23c58699e21edee6cd82984c4b754
Arik Fraimovich <arik@arikfr.com>
2015-07-20 07:32:13 +0300

    Bump version

--- redash/__init__.py
+++ redash/__init__.py
@@ -6,7 +6,7 @@ from statsd import StatsClient
 from redash import settings
 from redash.query_runner import import_query_runners
 
-__version__ = '0.6.4'
+__version__ = '0.7.0'
 
 
 def setup_logging():
